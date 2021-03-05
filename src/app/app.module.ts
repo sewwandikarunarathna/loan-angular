fb0441ab482e9387cd3c092e8946ddf36be0ffea
sewwandi karunarathna <sewwandi1104@gmail.com>
2021-03-05 16:29:41 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import {UserService} from './user.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserhomeComponent } from './userhome/userhome.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserhomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm:FormGroup = new FormGroup({
+    email:new FormControl(null,[Validators.email, Validators.required]),
+    password:new FormControl(null,Validators.required),
+  })
+  constructor(private _router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  moveToRegister(){
+    this._router.navigate(['/register']);
+  }
+
+  login(){
+    if(!this.loginForm.valid){
+      console.log("Invalid ");
+      return;
+    }
+
+    console.log(JSON.stringify(this.loginForm.value));
+  }
+}
